84fef5b6ec0bc24576c6b28ed18ea16b59c9adad
HitmanBoi <kamalkant.ug.21@ctuniversity.in>
2024-07-03 22:19:28 +0530

    Updated Webpage

--- Juan_Horse/src/Juan_Horse_frontend/src/App.jsx
+++ Juan_Horse/src/Juan_Horse_frontend/src/App.jsx
@@ -17,7 +17,13 @@ function App() {
     <main>
       <img src="/logo2.svg" alt="DFINITY logo" />
       <br />
-      
+      <br />
+      <form action="#" onSubmit={handleSubmit}>
+        <label htmlFor="name">Enter your name: &nbsp;</label>
+        <input id="name" alt="Name" type="text" />
+        <button type="submit">Click Me!</button>
+      </form>
+      <section id="greeting">{greeting}</section>
     </main>
   );
 }
