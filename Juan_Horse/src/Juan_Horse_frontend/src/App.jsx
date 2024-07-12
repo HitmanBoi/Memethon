ba2b62c5dcc9a69a72d9a45120eacde93845f2f1
HitmanBoi <kamalkant.ug.21@ctuniversity.in>
2024-07-12 22:18:32 +0530

    Updated Page

--- Juan_Horse/src/Juan_Horse_frontend/src/App.jsx
+++ Juan_Horse/src/Juan_Horse_frontend/src/App.jsx
@@ -39,7 +39,7 @@ const App = () => {
         <p>15% : Presale</p>
         <p>45% : In Game Rewards</p>
         <p>25% : Airdrops</p>
-        <p>10% : Community and Platform Development </p>
+        
         
         <img src="public/Horse_grazing.jpeg" alt="Horse Grazing" height={100} />
       </section>
